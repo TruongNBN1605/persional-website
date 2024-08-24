4668c3e69c33e8af0afa9b1d5e96b14ece4ec741
DevViTien <truongnbn.dev@gmail.com>
2024-08-24 09:14:46 +0700

    init commit

---
+++ tailwind.config.ts
@@ -0,0 +1,67 @@
+import type { Config } from "tailwindcss";
+const config: Config = {
+	darkMode: "class",
+	content: [
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+		// "./node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+		// "./node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				primary: {
+					"50": "#fff7ed",
+					"100": "#ffedd5",
+					"200": "#fed7aa",
+					"300": "#fdba74",
+					"400": "#fb923c",
+					"500": "#f97316",
+					"600": "#ea580c",
+					"700": "#c2410c",
+					"800": "#9a3412",
+					"900": "#7c2d12",
+					"950": "#431407",
+				},
+			},
+		},
+		fontFamily: {
+			body: [
+				"Open Sans",
+				"ui-sans-serif",
+				"system-ui",
+				"-apple-system",
+				"system-ui",
+				"Segoe UI",
+				"Roboto",
+				"Helvetica Neue",
+				"Arial",
+				"Noto Sans",
+				"sans-serif",
+				"Apple Color Emoji",
+				"Segoe UI Emoji",
+				"Segoe UI Symbol",
+				"Noto Color Emoji",
+			],
+			sans: [
+				"Open Sans",
+				"ui-sans-serif",
+				"system-ui",
+				"-apple-system",
+				"system-ui",
+				"Segoe UI",
+				"Roboto",
+				"Helvetica Neue",
+				"Arial",
+				"Noto Sans",
+				"sans-serif",
+				"Apple Color Emoji",
+				"Segoe UI Emoji",
+				"Segoe UI Symbol",
+				"Noto Color Emoji",
+			],
+		},
+	},
+	plugins: [],
+}
+export default config;
